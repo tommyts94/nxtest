e4e1c2291b53908a11791a75371a4adf022c865c
tommyts94 <166877861+tommyts94@users.noreply.github.com>
2024-04-13 22:19:37 +0300

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -95,7 +95,10 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
+  nixConfig = {
+    extra-substituters = ["https://hyprland.cachix.org"];
+    extra-trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
